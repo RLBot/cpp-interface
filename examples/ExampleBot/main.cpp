d397fb59d15b61b0ec770145ae96ed978cd2bf11
Michael Theall <pigman46@gmail.com>
2025-03-02 17:39:14 -0600

    Add RLBOT_SERVER_IP support

--- examples/ExampleBot/main.cpp
+++ examples/ExampleBot/main.cpp
@@ -32,6 +32,13 @@ int main (int argc_, char *argv_[])
 		return EXIT_FAILURE;
 	}
 
+	auto const serverHost = [] () -> char const * {
+		auto const env = std::getenv ("RLBOT_SERVER_IP");
+		if (env)
+			return env;
+		return "127.0.0.1";
+	}();
+
 	auto const serverPort = [] () -> char const * {
 		auto const env = std::getenv ("RLBOT_SERVER_PORT");
 		if (env)
@@ -39,7 +46,7 @@ int main (int argc_, char *argv_[])
 		return "23234";
 	}();
 
-	auto const host = argc_ > 1 ? argv_[1] : "127.0.0.1";
+	auto const host = argc_ > 1 ? argv_[1] : serverHost;
 	auto const port = argc_ > 2 ? argv_[2] : serverPort;
 
 	rlbot::BotManager<ExampleBot> manager{true};
